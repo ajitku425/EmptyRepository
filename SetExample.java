56ab65caa29fa548b75069d51ecf64fe97134959
ajitku425 <ajitkumar536@gmail.com>
2020-11-12 08:55:54 +0530

    first commit

---
+++ SetExample.java
@@ -0,0 +1,75 @@
+package com.collection.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Set<Employee> emp = new HashSet<>();
+		emp.add(new Employee(12,"Ajit"));
+		emp.add(new Employee(15,"Kumar"));
+		emp.add(new Employee(12,"Ajit"));
+		
+		System.out.println(emp);
+	}
+}
+
+
+class Employee{
+	
+	private int eid;
+	private String ename;
+	
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public Employee(int eid, String ename) {
+		super();
+		this.eid = eid;
+		this.ename = ename;
+	}
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + ", ename=" + ename + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + eid;
+		result = prime * result + ((ename == null) ? 0 : ename.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (eid != other.eid)
+			return false;
+		if (ename == null) {
+			if (other.ename != null)
+				return false;
+		} else if (!ename.equals(other.ename))
+			return false;
+		return true;
+	}
+	
+	
+}
